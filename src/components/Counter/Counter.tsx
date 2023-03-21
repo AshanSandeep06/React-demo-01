6316280e990f8c7ff4f26993b275ef60f644efdc
AshanSandeep06 <ashansandeep06@gmail.com>
2023-03-21 20:42:44 +0530

    Created the Counter Component

--- src/components/Counter/Counter.tsx
+++ src/components/Counter/Counter.tsx
@@ -3,14 +3,14 @@ import React, {Component} from "react";
 export default class Counter extends Component {
     render(): React.ReactNode {
         return (
-            <section>
+            <section className="mt-3.5 ml-4 flex flex-col gap-3">
                 <div>
                     <h1>Counter : <span>0</span></h1>
                 </div>
 
-                <div>
-                    <button>Increase</button>
-                    <button>Decrease</button>
+                <div className="text-white flex gap-3">
+                    <button className="rounded bg-green-800 px-3 py-1.5">Increase</button>
+                    <button className="rounded bg-red-700 px-3 py-1.5">Decrease</button>
                 </div>
             </section>
         );
