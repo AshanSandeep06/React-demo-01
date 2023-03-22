c69b33bff4a13bf8a05cccfe39e0e6deac706858
AshanSandeep06 <ashansandeep06@gmail.com>
2023-03-22 16:34:06 +0530

    Content Updated in Counter Component

--- src/components/Counter/Counter.tsx
+++ src/components/Counter/Counter.tsx
@@ -4,11 +4,12 @@ export default class Counter extends Component {
     render(): React.ReactNode {
         return (
             <section className="mt-3.5 ml-4 flex flex-col gap-3">
-                <div>
-                    <h1>Counter : <span>0</span></h1>
+                <div className="flex gap-2.5">
+                    <h1>Counter : </h1>
+                    <span>0</span>
                 </div>
 
-                <div className="text-white flex gap-3">
+                <div className="text-white flex gap-4">
                     <button className="rounded bg-green-800 px-3 py-1.5">Increase</button>
                     <button className="rounded bg-red-700 px-3 py-1.5">Decrease</button>
                 </div>
