0df79f2da7b7cd271be5b5563a26e2917757b973
AshanSandeep06 <ashansandeep06@gmail.com>
2023-03-23 18:02:46 +0530

    Created path mappings for Header's Options

--- src/components/Content/Content.tsx
+++ src/components/Content/Content.tsx
@@ -1,7 +1,15 @@
+import {Route, Routes} from "react-router-dom";
+
 export default function Content() {
     return (
         <section id="content">
-
+            <Routes>
+                <Route path="/"></Route>
+                <Route path="/About"></Route>
+                <Route path="/Services"></Route>
+                <Route path="/Projects"></Route>
+                <Route path="/Contact"></Route>
+            </Routes>
         </section>
     );
 }
