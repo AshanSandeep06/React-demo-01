6e81c50a2050a316f014c70b712cb735fa228cc1
AshanSandeep06 <ashansandeep06@gmail.com>
2023-03-23 12:17:00 +0530

    Content Updated With refactoring Counter Component

--- src/components/Counter/Counter.tsx
+++ src/components/Counter/Counter.tsx
@@ -7,12 +7,24 @@ type counterState = {
 }
 
 export default class Counter extends Component<counterProps, counterState> {
-    constructor(props: counterProps){
+    constructor(props: counterProps) {
         super(props);
 
         this.state = {
             count: 0
         };
+
+        this.increaseCount = this.increaseCount.bind(this);
+    }
+
+    // Normal Functions / Traditional Functions
+    increaseCount() {
+        this.setState({count: this.state.count + 1});
+    }
+
+    // Arrow Functions
+    decreaseCount = () => {
+        this.setState({count: this.state.count - 1});
     }
 
     render(): React.ReactNode {
@@ -20,12 +32,12 @@ export default class Counter extends Component<counterProps, counterState> {
             <section className="mt-3.5 ml-4 flex flex-col gap-3">
                 <div className="flex gap-2.5">
                     <h1>Counter : </h1>
-                    <span>0</span>
+                    <span>{this.state.count}</span>
                 </div>
 
                 <div className="text-white flex gap-4">
-                    <button className="rounded bg-green-800 px-3 py-1.5">Increase</button>
-                    <button className="rounded bg-red-700 px-3 py-1.5">Decrease</button>
+                    <button onClick={this.increaseCount} className="rounded bg-green-800 px-3 py-1.5">Increase</button>
+                    <button onClick={this.decreaseCount} className="rounded bg-red-700 px-3 py-1.5">Decrease</button>
                 </div>
             </section>
         );
