572106423aa8ccbd5c5ab33253640546de7e8229
AshanSandeep06 <ashansandeep06@gmail.com>
2023-03-23 17:31:19 +0530

    Content Updated in Header Component

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -2,7 +2,7 @@ import UserImage from "../../assets/UserImage.png";
 
 export default function Header() {
     return (
-        <section id="header" className="flex w-full h-20 bg-blue-900 text-white">
+        <section id="header" className="flex w-full h-20 bg-blue-800 shadow-lg shadow-blue-500/50 text-white">
             <div className="w-1/2 h-full flex items-center gap-2.5 pl-2">
                 <img src={UserImage} alt="UserImage" className="w-10 h-10"/>
                 <h1 className="h-max">My Profile</h1>
