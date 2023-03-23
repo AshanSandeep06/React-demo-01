3b2f8c0b951039cfb1078625bc494e84b868ba43
AshanSandeep06 <ashansandeep06@gmail.com>
2023-03-23 17:14:39 +0530

    Fully Refactored Header Component

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -3,13 +3,13 @@ import UserImage from "../../assets/UserImage.png";
 export default function Header() {
     return (
         <section id="header" className="flex w-full h-20 bg-blue-900 text-white">
-            <div className="w-1/2 h-full border border-4 border-red-800 flex items-center gap-2 pl-2">
+            <div className="w-1/2 h-full flex items-center gap-2.5 pl-2">
                 <img src={UserImage} alt="UserImage" className="w-10 h-10"/>
                 <h1 className="h-max">My Profile</h1>
             </div>
 
-            <div className="w-1/2 h-full border border-4 border-black">
-                <ul>
+            <div className="w-1/2 h-full pr-5">
+                <ul className="h-full flex items-center justify-end gap-[60px]">
                     <li>Home</li>
                     <li>About</li>
                     <li>Contact</li>
