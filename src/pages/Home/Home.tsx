d2b21e68ec6ea3a555be85aef057616bcedf7c26
AshanSandeep06 <ashansandeep06@gmail.com>
2023-03-26 17:42:50 +0530

    Content Updated

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,13 +1,13 @@
-import React, {Component} from 'react';
+import React, { Component } from "react";
 
 class Home extends Component {
-    render() {
-        return (
-            <section>
-                <h1>Home</h1>
-            </section>
-        );
-    }
+  render() {
+    return (
+      <section>
+        <h1>Home</h1>
+      </section>
+    );
+  }
 }
 
 export default Home;
