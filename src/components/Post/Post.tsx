25bb53bdc6e2136969df3321bcce223a21e0a2ff
AshanSandeep06 <ashansandeep06@gmail.com>
2023-03-27 11:11:43 +0530

    Refatored Post Component into Class Component

--- src/components/Post/Post.tsx
+++ src/components/Post/Post.tsx
@@ -1,33 +1,37 @@
-export default function Post() {
-  return (
-    <div className="border border-gray-600 mt-3">
-      <h3 className="text-center mt-2.5 text-xl font-semibold font-ubuntu">
-        Lecture Day-01
-      </h3>
+import { Component } from "react";
 
-      <p className="text-center my-3 mx-5 font-poppins font-light">
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Esse, id
-        maiores vel ab laborum at animi quia soluta, dolor nobis dolore possimus
-        itaque perferendis fugit deleniti alias odio molestiae odit.Lorem ipsum
-        dolor sit amet consectetur adipisicing elit. Esse, id maiores vel ab
-        laborum at animi quia soluta, dolor nobis dolore possimus itaque
-        perferendis fugit deleniti alias odio molestiae odit.Lorem ipsum dolor
-        sit amet consectetur adipisicing elit. Esse, id maiores vel ab laborum
-        at animi quia soluta, dolor nobis dolore possimus itaque perferendis
-        fugit deleniti alias odio molestiae odit.Lorem ipsum dolor sit amet
-        consectetur adipisicing elit. Esse, id maiores vel ab laborum at animi
-        quia soluta, dolor nobis dolore possimus itaque perferendis fugit
-        deleniti alias odio molestiae odit.
-      </p>
+export default class Post extends Component {
+  render(): React.ReactNode {
+    return (
+      <div className="border border-gray-600 mt-3">
+        <h3 className="text-center mt-2.5 text-xl font-semibold font-ubuntu">
+          Lecture Day-01
+        </h3>
 
-      <aside className="mb-5 mx-5 flex space-x-5 pt-2">
-        <span className="font-poppins text-sm font-light text-center border border-slate-400 rounded px-6 py-2">
-          Introduction
-        </span>
-        <span className="font-poppins text-sm font-light text-center border border-slate-400 rounded px-6 py-2">
-          Type-Script
-        </span>
-      </aside>
-    </div>
-  );
+        <p className="text-center my-3 mx-5 font-poppins font-light">
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Esse, id
+          maiores vel ab laborum at animi quia soluta, dolor nobis dolore
+          possimus itaque perferendis fugit deleniti alias odio molestiae
+          odit.Lorem ipsum dolor sit amet consectetur adipisicing elit. Esse, id
+          maiores vel ab laborum at animi quia soluta, dolor nobis dolore
+          possimus itaque perferendis fugit deleniti alias odio molestiae
+          odit.Lorem ipsum dolor sit amet consectetur adipisicing elit. Esse, id
+          maiores vel ab laborum at animi quia soluta, dolor nobis dolore
+          possimus itaque perferendis fugit deleniti alias odio molestiae
+          odit.Lorem ipsum dolor sit amet consectetur adipisicing elit. Esse, id
+          maiores vel ab laborum at animi quia soluta, dolor nobis dolore
+          possimus itaque perferendis fugit deleniti alias odio molestiae odit.
+        </p>
+
+        <aside className="mb-5 mx-5 flex space-x-5 pt-2">
+          <span className="font-poppins text-sm font-light text-center border border-slate-400 rounded px-6 py-2">
+            Introduction
+          </span>
+          <span className="font-poppins text-sm font-light text-center border border-slate-400 rounded px-6 py-2">
+            Type-Script
+          </span>
+        </aside>
+      </div>
+    );
+  }
 }
--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,9 +1,13 @@
 import React, { Component } from "react";
+import Post from "../../components/Post";
 
 class Home extends Component {
   render() {
     return (
       <section className="px-12 pt-[18px] pb-10 flex flex-col gap-y-4">
+        <Post />
+        <Post />
+        <Post />
       </section>
     );
   }
