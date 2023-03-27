9fcf57e6710da5569ab9345de198f9eb877cf5c3
AshanSandeep06 <ashansandeep06@gmail.com>
2023-03-27 10:19:07 +0530

    Content Updated

--- src/components/Content/Content.tsx
+++ src/components/Content/Content.tsx
@@ -1,4 +1,4 @@
-import {Route, Routes} from "react-router-dom";
+import { Route, Routes } from "react-router-dom";
 import "./Content.css";
 import Home from "../../pages/Home";
 import About from "../../pages/About";
@@ -7,17 +7,17 @@ import Projects from "../../pages/Projects";
 import Services from "../../pages/Services";
 
 export default function Content() {
-    return (
-        <>
-            <section id="content" className="mt-20 flex justify-center items-center">
-                <Routes>
-                    <Route path="/" element={<Home/>}></Route>
-                    <Route path="/About" element={<About/>}></Route>
-                    <Route path="/Services" element={<Services/>}></Route>
-                    <Route path="/Projects" element={<Projects/>}></Route>
-                    <Route path="/Contact" element={<Contact/>}></Route>
-                </Routes>
-            </section>
-        </>
-    );
+  return (
+    <>
+      <section id="content" className="mt-20 flex justify-center items-center">
+        <Routes>
+          <Route path="/" element={<Home />}></Route>
+          <Route path="/About" element={<About />}></Route>
+          <Route path="/Contact" element={<Contact />}></Route>
+          <Route path="/Services" element={<Services />}></Route>
+          <Route path="/Projects" element={<Projects />}></Route>
+        </Routes>
+      </section>
+    </>
+  );
 }
--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,26 +1,54 @@
-import {NavLink} from "react-router-dom";
+import { NavLink } from "react-router-dom";
 import UserImage from "../../assets/UserImage.png";
 
 export default function Header() {
-    const activeLink = "border-b-2";
-    const normalLink = "";
+  const activeLink = "border-b-2";
+  const normalLink = "";
 
-    return (
-        <section id="header" className="flex w-full h-20 bg-blue-800 shadow-lg shadow-black/20 text-white fixed top-0">
-            <div className="w-1/2 h-full flex items-center gap-2.5 pl-2">
-                <img src={UserImage} alt="UserImage" className="w-10 h-10"/>
-                <h1 className="h-max mb-[3px]">My Profile</h1>
-            </div>
+  return (
+    <section
+      id="header"
+      className="flex w-full h-20 bg-blue-800 shadow-lg shadow-black/20 text-white fixed top-0"
+    >
+      <div className="w-1/2 h-full flex items-center gap-2.5 pl-2">
+        <img src={UserImage} alt="UserImage" className="w-10 h-10" />
+        <h1 className="h-max mb-[3px]">My Profile</h1>
+      </div>
 
-            <div className="w-1/2 h-full pr-5">
-                <ul className="h-full flex items-center justify-end gap-[60px]">
-                    <NavLink to={"/"} className={({isActive}) => isActive ? activeLink : normalLink}>Home</NavLink>
-                    <NavLink to={"/About"} className={({isActive}) => isActive ? activeLink : normalLink}>About</NavLink>
-                    <NavLink to={"/Services"} className={({isActive}) => isActive ? activeLink : normalLink}>Services</NavLink>
-                    <NavLink to={"/Projects"} className={({isActive}) => isActive ? activeLink : normalLink}>Projects</NavLink>
-                    <NavLink to={"/Contact"} className={({isActive}) => isActive ? activeLink : normalLink}>Contact</NavLink>
-                </ul>
-            </div>
-        </section>
-    );
+      <div className="w-1/2 h-full pr-5">
+        <ul className="h-full flex items-center justify-end gap-[60px]">
+          <NavLink
+            to={"/"}
+            className={({ isActive }) => (isActive ? activeLink : normalLink)}
+          >
+            Home
+          </NavLink>
+          <NavLink
+            to={"/About"}
+            className={({ isActive }) => (isActive ? activeLink : normalLink)}
+          >
+            About
+          </NavLink>
+          <NavLink
+            to={"/Contact"}
+            className={({ isActive }) => (isActive ? activeLink : normalLink)}
+          >
+            Contact
+          </NavLink>
+          <NavLink
+            to={"/Services"}
+            className={({ isActive }) => (isActive ? activeLink : normalLink)}
+          >
+            Services
+          </NavLink>
+          <NavLink
+            to={"/Projects"}
+            className={({ isActive }) => (isActive ? activeLink : normalLink)}
+          >
+            Projects
+          </NavLink>
+        </ul>
+      </div>
+    </section>
+  );
 }
