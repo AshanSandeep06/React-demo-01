6b46ccad8270f2aff2e73173e235f7a49dc5b38e
AshanSandeep06 <ashansandeep06@gmail.com>
2023-03-27 20:01:23 +0530

    Fully Refactored Login Form

--- src/pages/Login/Login.tsx
+++ src/pages/Login/Login.tsx
@@ -1,12 +1,15 @@
 import { TextField } from "@mui/material";
 import React, { Component } from "react";
+import LoginIcon from "@mui/icons-material/Login";
 
 export default class Login extends Component {
   render() {
     return (
-      <section>
-        <form className="w-full py-8 px-32 flex flex-col gap-3 items-center">
-          <div className="w-full px-16 flex flex-col gap-4 items-center">
+      <section className="h-full">
+        <form className="w-full h-full py-8 px-32 flex flex-col gap-3 items-center justify-center">
+          <div className="pt-5 w-1/2 px-16 flex flex-col gap-4 items-center shadow-xl h-[300px] rounded-xl">
+            <h1>Login Form</h1>
+
             <TextField
               label="Username"
               type="text"
@@ -25,8 +28,8 @@ export default class Login extends Component {
               required={true}
             />
 
-            <button className="mt-2 w-1/6 bg-blue-900 text-white py-2 rounded">
-              <h6>Login</h6>
+            <button className="mt-2 bg-blue-900 text-white py-2 px-6 rounded">
+              Login <LoginIcon />
             </button>
           </div>
         </form>
