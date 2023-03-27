7209be9f93ba3bdddc07259db70297b3dc66a295
AshanSandeep06 <ashansandeep06@gmail.com>
2023-03-27 16:07:02 +0530

    Refactored About Page

--- src/pages/About/About.tsx
+++ src/pages/About/About.tsx
@@ -1,37 +1,34 @@
 import React, { Component } from "react";
+import AcUnitIcon from "@mui/icons-material/AcUnit";
+import AddAlertIcon from "@mui/icons-material/AddAlert";
+import AddCircleIcon from "@mui/icons-material/AddCircle";
+import CheckCircleIcon from "@mui/icons-material/CheckCircle";
 
 class About extends Component {
   render() {
     return (
       <section className="px-12 pt-[18px] pb-10 flex flex-col gap-y-4">
-        <div>
-          <h1>About Us</h1>
+        <div className="mt-2 mb-3">
+          <h1 className="text-center font-bold text-2xl font-poppins">
+            About Us
+          </h1>
         </div>
 
-        <div>
-          <p>
+        <div className="mb-6 flex justify-center">
+          <p className="w-1/2">
             Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestias
             excepturi alias repellendus temporibus tempora explicabo, ducimus
             nisi animi voluptas atque rerum nam odit aut. Voluptate facere
-            placeat libero molestiae nostrum!Lorem ipsum dolor sit amet
-            consectetur adipisicing elit. Molestias excepturi alias repellendus
-            temporibus tempora explicabo, ducimus nisi animi voluptas atque
-            rerum nam odit aut. Voluptate facere placeat libero molestiae
-            nostrum!Lorem ipsum dolor sit amet consectetur adipisicing elit.
-            Molestias excepturi alias repellendus temporibus tempora explicabo,
-            ducimus nisi animi voluptas atque rerum nam odit aut. Voluptate
-            facere placeat libero molestiae nostrum!Lorem ipsum dolor sit amet
-            consectetur adipisicing elit. Molestias excepturi alias repellendus
-            temporibus tempora explicabo, ducimus nisi animi voluptas atque
-            rerum nam odit aut. Voluptate facere placeat libero molestiae
-            nostrum!
+            placeat libero molestiae nostrum!
           </p>
         </div>
 
-        <div className="grid grid-cols-3 gap-8">
+        <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-8">
           <div className="border border-gray-600 text-center px-4 pb-5 pt-3">
-            <div>
-              <h3 className="text-center text-xl font-medium font-ubuntu mb-2">
+            <div className="flex flex-col items-center gap-2">
+              <AcUnitIcon />
+
+              <h3 className="text-center text-2xl font-medium font-ubuntu mb-2">
                 Card 1
               </h3>
             </div>
@@ -47,9 +44,49 @@ class About extends Component {
           </div>
 
           <div className="border border-gray-600 text-center px-4 pb-5 pt-3">
-            <div>
-              <h3 className="text-center text-xl font-medium font-ubuntu mb-2">
+            <div className="flex flex-col items-center gap-2">
+              <AddAlertIcon />
+
+              <h3 className="text-center text-2xl font-medium font-ubuntu mb-2">
                 Card 2
+              </h3>
+            </div>
+
+            <div className="flex justify-center">
+              <p>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Ex
+                velit, facilis quaerat accusamus tenetur laborum ipsam nemo
+                doloribus sequi mollitia aut ullam dolores tempora quos
+                veritatis quasi aliquid libero ea.
+              </p>
+            </div>
+          </div>
+
+          <div className="border border-gray-600 text-center px-4 pb-5 pt-3">
+            <div className="flex flex-col items-center gap-2">
+              <AddCircleIcon />
+
+              <h3 className="text-center text-2xl font-medium font-ubuntu mb-2">
+                Card 3
+              </h3>
+            </div>
+
+            <div className="flex justify-center">
+              <p>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Ex
+                velit, facilis quaerat accusamus tenetur laborum ipsam nemo
+                doloribus sequi mollitia aut ullam dolores tempora quos
+                veritatis quasi aliquid libero ea.
+              </p>
+            </div>
+          </div>
+
+          <div className="border border-gray-600 text-center px-4 pb-5 pt-3">
+            <div className="flex flex-col items-center gap-2">
+              <CheckCircleIcon />
+
+              <h3 className="text-center text-2xl font-medium font-ubuntu mb-2">
+                Card 4
               </h3>
             </div>
 
