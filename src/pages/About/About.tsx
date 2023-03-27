a93f52def8b0a638ba4dfa586cafa5ddcd6647a9
AshanSandeep06 <ashansandeep06@gmail.com>
2023-03-27 16:09:42 +0530

    Content Updated in About Page

--- src/pages/About/About.tsx
+++ src/pages/About/About.tsx
@@ -24,24 +24,7 @@ class About extends Component {
         </div>
 
         <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-8">
-          <div className="border border-gray-600 text-center px-4 pb-5 pt-3">
-            <div className="flex flex-col items-center gap-2">
-              <AcUnitIcon />
-
-              <h3 className="text-center text-2xl font-medium font-ubuntu mb-2">
-                Card 1
-              </h3>
-            </div>
-
-            <div className="flex justify-center">
-              <p>
-                Lorem ipsum dolor sit amet consectetur adipisicing elit. Ex
-                velit, facilis quaerat accusamus tenetur laborum ipsam nemo
-                doloribus sequi mollitia aut ullam dolores tempora quos
-                veritatis quasi aliquid libero ea.
-              </p>
-            </div>
-          </div>
+          
         </div>
       </section>
     );
