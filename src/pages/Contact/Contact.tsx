5085051e4fa1630be93767dc2ed6ab39fa6a75e2
AshanSandeep06 <ashansandeep06@gmail.com>
2023-03-27 16:51:16 +0530

    Content Updated in Contact Page

--- src/pages/Contact/Contact.tsx
+++ src/pages/Contact/Contact.tsx
@@ -1,13 +1,40 @@
-import React, {Component} from 'react';
+import React, { Component } from "react";
 
 class Contact extends Component {
-    render() {
-        return (
-            <section>
-                <h1>Contact</h1>
-            </section>
-        );
-    }
+  render() {
+    return (
+      <section className="px-12 pt-[18px] pb-10 flex flex-col gap-y-4">
+        <div className="mt-2 mb-3">
+          <h1 className="text-center text-2xl font-semibold font-poppins">
+            Contact Us
+          </h1>
+        </div>
+
+        <div className="mb-6 flex justify-center px-6">
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestias
+            excepturi alias repellendus temporibus tempora explicabo, ducimus
+            nisi animi voluptas atque rerum nam odit aut. Voluptate facere
+            placeat libero molestiae nostrum!Lorem ipsum dolor sit amet
+            consectetur adipisicing elit. Molestias excepturi alias repellendus
+            temporibus tempora explicabo, ducimus nisi animi voluptas atque
+            rerum nam odit aut. Voluptate facere placeat libero molestiae
+            nostrum!Lorem ipsum dolor sit amet consectetur adipisicing elit.
+            Molestias excepturi alias repellendus temporibus tempora explicabo,
+            ducimus nisi animi voluptas atque rerum nam odit aut. Voluptate
+            facere placeat libero molestiae nostrum!Lorem ipsum dolor sit amet
+            consectetur adipisicing elit. Molestias excepturi alias repellendus
+            temporibus tempora explicabo, ducimus nisi animi voluptas atque
+            rerum nam odit aut. Voluptate facere placeat libero molestiae
+            nostrum!Lorem ipsum dolor sit amet consectetur adipisicing elit.
+            Molestias excepturi alias repellendus temporibus tempora explicabo,
+            ducimus nisi animi voluptas atque rerum nam odit aut. Voluptate
+            facere placeat libero molestiae nostrum!
+          </p>
+        </div>
+      </section>
+    );
+  }
 }
 
 export default Contact;
