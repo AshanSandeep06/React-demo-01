489c947c0b5d38fb6d4028436f8c356a2f288b52
AshanSandeep06 <ashansandeep06@gmail.com>
2023-03-27 10:22:10 +0530

    Content Updated in Home.tsx

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -3,9 +3,9 @@ import React, { Component } from "react";
 class Home extends Component {
   render() {
     return (
-      <section>
-        
-      </section>
+    <section>
+      
+    </section>
     );
   }
 }
